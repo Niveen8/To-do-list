ad6ef39da5004b094b72f6d87d24bb84ad0e58dd
Niveen8 <niveenabutamam@gmail.com>
2025-06-07 02:54:08 +0300

    add value if emmpty

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ function dateTodo() {
 const todoInput = document.getElementById('newTodo');
 const errorMessage = document.getElementById('error message');
 const value = todoInput.value.trim();
+if (value === '') {
+        errorMessage.textContent = '⛔ Task cannot be empty';
+        return false;
+    }
+    
+
 }
