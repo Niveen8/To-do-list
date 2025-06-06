efaff84b872bdb4a130657c6cc841859790b4898
Niveen8 <niveenabutamam@gmail.com>
2025-06-07 02:59:19 +0300

    add if ,massge

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const todoInput = document.getElementById('newTodo');
 const errorMessage = document.getElementById('error message');
 const value = todoInput.value.trim();
 if (value === '') {
-        errorMessage.textContent = ' Task cannot be empty';
+        errorMessage.textContent = 'Must contain a search and not be empty';
         return false;
     }
-
+if (taskText.length < 10) {
+        errorMessage.textContent = 'Your search must contain at least 10 characters.';
+        return false;
+    }
 
 }
