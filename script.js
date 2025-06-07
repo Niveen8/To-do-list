ab2068d8ca1da2f72cc22b9c027156f4d78f6416
Niveen8 <niveenabutamam@gmail.com>
2025-06-08 02:26:02 +0300

    addtodo

--- script.js
+++ script.js
@@ -8,7 +8,15 @@ function saveTodos() {
     localStorage.setItem("todos", JSON.stringify(todos));
   }
   const input = document.getElementById("New-Todo");
-  const addBtn = document.querySelector("button"); // زر "add new task"
+  const addBtn = document.querySelector("button"); /*زر "add new task"*/
   const section = document.querySelector(".todo-section");
   const filterButtons = document.querySelectorAll(".filter-buttons .btn");
-  const noTasksMessage = document.querySelector(".no-task
+  const noTasksMessage = document.querySelector(".no-tasks");
+  addBtn.addEventListener("click", addTodo);
+
+  function addTodo() {
+    const taskText = input.value.trim();
+    if (taskText === "") {
+      document.getElementById("error-message").textContent = "Please enter a task.";
+      return;
+    }}
