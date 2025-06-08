674d01dbafc49db4e379cbac5025a5b38e946301
Hebashannak <h.shannak@student.aaup.edu>
2025-06-08 21:27:33 +0300

    js2

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function deleteDoneTodos() {
     document.getElementById("deleteAllDoneModal").style.display = "block";
   }
-  
+
+    function confirmDeleteAllDone() {
+    todos = todos.filter(todo => !todo.done);
+    document.getElementById("deleteAllDoneModal").style.display = "none";
+    saveTodos();  
+    renderTodos(currentFilter);
+  }
